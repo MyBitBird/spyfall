7130d4ef6e91fbd77200db9e9dc716b2a2afb3a2
Meisam Malekzadeh <meisammalekzadeh@gmail.com>
2020-06-29 23:36:29 +0430

    api primary added

---
+++ src/services/api.ts
@@ -0,0 +1,35 @@
+import axios, { AxiosError, AxiosResponse, AxiosRequestConfig } from "axios";
+
+const instance = axios.create();
+let onLoadingChanged: (status: boolean) => void = () => {};
+
+instance.defaults.baseURL = 'lcalhost:5000/api/';
+
+instance.interceptors.request.use((config: AxiosRequestConfig) => {
+  onLoadingChanged(true);
+  return config;
+});
+
+instance.interceptors.response.use(
+  function(response: AxiosResponse) {
+    onLoadingChanged(false);
+    return response;
+  },
+  function(error: AxiosError) {
+    onLoadingChanged(false);
+    //TODO lging errors
+    return Promise.reject(error);
+  },
+);
+
+export const onLoadingChangedEvent = (callback: (status: boolean) => void) => {
+  onLoadingChanged = callback;
+};
+
+export default {
+  get: instance.get,
+  post: instance.post,
+  put: instance.put,
+  delete: instance.delete,
+  patch: instance.patch,
+};
