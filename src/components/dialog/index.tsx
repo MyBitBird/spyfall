d1cf8ba904b1500a5e0def98f20ddb990ee91d5c
Meisam Malekzadeh <meisammalekzadeh@gmail.com>
2020-06-29 11:27:36 +0430

    dialog component completed

--- src/components/dialog/index.tsx
+++ src/components/dialog/index.tsx
@@ -1,12 +1,19 @@
 import * as React from "react";
-import { Dialog, DialogTitle, DialogContent } from "@material-ui/core";
+import {
+  Dialog,
+  DialogTitle,
+  DialogContent,
+  DialogActions,
+  Button,
+} from "@material-ui/core";
 import { useTheme } from "@material-ui/core/styles";
 import useMediaQuery from "@material-ui/core/useMediaQuery";
 
 export interface DialogProps {
   isOpen: boolean;
-  onClose(): void;
   title: String;
+  approve :{title: string , action(): void}
+  close :{title: string , action(): void}
 }
 
 const FullDialog: React.SFC<DialogProps> = (props) => {
@@ -14,9 +21,17 @@ const FullDialog: React.SFC<DialogProps> = (props) => {
   const fullScreen = useMediaQuery(theme.breakpoints.down("sm"));
 
   return (
-    <Dialog open={props.isOpen} fullScreen={fullScreen} onClose={props.onClose}>
+    <Dialog open={props.isOpen} fullScreen={fullScreen} onClose={props.close.action}>
       <DialogTitle>{props.title}</DialogTitle>
       <DialogContent>{props.children}</DialogContent>
+      <DialogActions>
+        <Button onClick={props.approve.action} color="secondary">
+          {props.approve.title}
+        </Button>
+        <Button onClick={props.close.action} color="primary">
+          {props.close.title}
+        </Button>
+      </DialogActions>
     </Dialog>
   );
 };
