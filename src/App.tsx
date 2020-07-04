c21170fcc6f208f4ea86fd3fed76792e1b7d2541
Meisam Malekzadeh <meisammalekzadeh@gmail.com>
2020-07-05 00:52:56 +0430

    using room provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,20 @@
 import React from "react";
 import { CssBaseline } from "@material-ui/core";
-import { Route, Switch } from "react-router";
+import {Route,Switch} from 'react-router-dom'
 import routes from "./routes";
+import RoomProvider from './providers/room'
 
 function App() {
   return (
     <div className="App">
       <CssBaseline />
-      <Switch>
-        {routes.map((route, key) => {
-          return <Route key={key} {...route} />;
-        })}
-      </Switch>
+      <RoomProvider>
+        <Switch>
+          {routes.map((route, key) => {
+            return <Route key={key} {...route} />;
+          })}
+        </Switch>
+      </RoomProvider>
     </div>
   );
 }
