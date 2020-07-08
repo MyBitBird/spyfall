088b38ddc053792d6e9e12da11c39ac39b1b7eff
Meisam Malekzadeh <meisammalekzadeh@gmail.com>
2020-07-09 01:05:49 +0430

    joinRoom added

--- src/services/roomService.ts
+++ src/services/roomService.ts
@@ -7,3 +7,10 @@ export const createRoom = async (name: string) => {
   const result = await api.post(SUB_URL, { name: name });
   return result.data as Room;
 };
+
+export const joinRoom = async (name: string , code : string) =>
+{
+  const result = await api.patch(`${SUB_URL}/${code}`, { name: name});
+  return result.data as Room;
+
+}
