b2545aa0e2c35d85664a2a8d822a30200acc348c
Meisam Malekzadeh <meisammalekzadeh@gmail.com>
2020-07-12 17:14:19 +0430

    adding game hook

--- src/App.tsx
+++ src/App.tsx
@@ -3,17 +3,20 @@ import { CssBaseline } from "@material-ui/core";
 import {Route,Switch} from 'react-router-dom'
 import routes from "./routes";
 import RoomProvider from './providers/room'
+import GameProvider from './providers/game';
 
 function App() {
   return (
     <div className="App">
       <CssBaseline />
       <RoomProvider>
+        <GameProvider>
         <Switch>
           {routes.map((route, key) => {
             return <Route key={key} {...route} />;
           })}
         </Switch>
+        </GameProvider>
       </RoomProvider>
     </div>
   );
