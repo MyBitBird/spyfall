1ceb36f18d9448a82a77d4077ac710efa4e97906
Meisam Malekzadeh <meisammalekzadeh@gmail.com>
2020-07-16 01:11:17 +0430

    making room page a little better

--- src/pages/room/index.tsx
+++ src/pages/room/index.tsx
@@ -4,14 +4,13 @@ import useRoom from "./../../hooks/room/useRoom";
 import { connect, addEvent, disconnect } from "../../socket";
 import { Player } from "./../../types/player";
 import Config from "../../config";
-import FaceIcon from "@material-ui/icons/Face";
-import DeleteIcon from "@material-ui/icons/Delete";
+import CloseIcon from "@material-ui/icons/HighlightOff";
 import useStyles from "./style";
 import { useHistory } from "react-router-dom";
-import { getRoom,leaveRoom } from "../../services/roomService";
+import { getRoom, leaveRoom } from "../../services/roomService";
 import useRoomAction from "./../../hooks/room/useRoomActions";
 import useGameAction from "./../../hooks/game/useGameAction";
-import {startGame} from '../../services/gameService'
+import { startGame } from "../../services/gameService";
 
 export interface PageProps {}
 
@@ -25,25 +24,22 @@ const Page: React.SFC<PageProps> = () => {
   const [players, setPlayers] = useState<Player[]>([]);
 
   const onPlayersChanged = (data: any) => {
-    console.log("socket recive", data);
     setPlayers(players.concat(data));
   };
 
-  const onGameStarted = (game : any) => {
-    console.log('recieved game' , game)
+  const onGameStarted = (game: any) => {
     setGame(game);
     history.push("/game");
   };
 
-  const onStartGame = async () =>
-  {
+  const onStartGame = async () => {
     await startGame();
-  }
+  };
 
-  const handleLeaveRoom = async () =>{
+  const handleLeaveRoom = async () => {
     await leaveRoom();
-    history.push('/')
-  }
+    history.push("/");
+  };
 
   useEffect(() => {
     const loadRoom = async () => {
@@ -64,44 +60,49 @@ const Page: React.SFC<PageProps> = () => {
   return (
     <Grid container>
       <Grid item md={4} xs={false} />
-      <Grid item md={4} xs={12}>
-        <Paper className={classes.container}>
-          <Typography variant="h5" component="h5">
-            Join Code: {room?.code}
-          </Typography>
-          <hr />
-          <Grid container>
-            {players.map((player: any, key: any) => {
-              return (
-                <Grid item md={12} xs={12}  key={key}>
-                  <Chip
-                    className={classes.player}
-                    icon={<FaceIcon />}
-                    label={player.name}
-                    clickable
-                    color="secondary"
-                    onDelete={() => {}}
-                    deleteIcon={<DeleteIcon />}
-                  />
-                </Grid>
-              );
-            })}
-            <Grid item md={6} xs={12}>
-              <Button
-                variant="contained"
-                color="primary"
-                onClick={onStartGame}
-              >
-                Start Game
-              </Button>
-            </Grid>
-            <Grid item md={6} xs={12}>
-              <Button variant="contained" color="primary" onClick={handleLeaveRoom}>
-                Leave Room{" "}
-              </Button>
-            </Grid>
+      <Grid item md={4} xs={12} className={classes.container}>
+        <Typography variant="h5" component="h5">
+          Join Code: {room?.code}
+        </Typography>
+        <hr />
+        <Grid container spacing={1}>
+          {players.map((player: any, key: any) => {
+            return (
+              <Grid item md={12} xs={12} key={key}>
+                <Paper className={classes.player}>
+                  <div className={classes.playerTitle}>
+                    <label> {`${key + 1}.${player.name}`}</label>
+                  </div>
+                  <div className={classes.deleteButton}>
+                    <CloseIcon color="secondary" onClick={() => {}} />
+                  </div>
+                </Paper>
+              </Grid>
+            );
+          })}
+        </Grid>
+        <Grid container className={classes.buttons} spacing={2}>
+          <Grid item md={6} xs={12}>
+            <Button
+              variant="contained"
+              fullWidth
+              color="primary"
+              onClick={onStartGame}
+            >
+              Start Game
+            </Button>
           </Grid>
-        </Paper>
+          <Grid item md={6} xs={12}>
+            <Button
+              variant="contained"
+              color="primary"
+              fullWidth
+              onClick={handleLeaveRoom}
+            >
+              Leave Room{" "}
+            </Button>
+          </Grid>
+        </Grid>
       </Grid>
       <Grid item md={4} xs={false} />
     </Grid>
--- src/pages/room/style.tsx
+++ src/pages/room/style.tsx
@@ -1,14 +1,24 @@
-import { makeStyles } from '@material-ui/core/styles';
+import { makeStyles } from "@material-ui/core/styles";
 
-const useStyles = makeStyles(theme => ({
-    container : {
-        textAlign:'center',
-        padding: '10px',
-    },
-    player : {
-        margin:5,
-        width: '100%'
-    },
-  }));
+const useStyles = makeStyles((theme) => ({
+  container: {
+    textAlign: "center",
+  },
+  player: {
+    textAlign: "center",
+    padding: "15px",
+    display: "flex",
+  },
+  playerTitle: {
+    textAlign: "left",
+    flex: 1,
+  },
+  deleteButton: {
+    cursor: "pointer",
+  },
+  buttons: {
+    marginTop: 20,
+  },
+}));
 
-  export default useStyles;
+export default useStyles;
