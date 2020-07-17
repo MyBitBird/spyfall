3e918935972214a90463712435c9a530b2a6424c
Meisam Malekzadeh <meisammalekzadeh@gmail.com>
2020-07-17 18:37:41 +0430

    toasting api errors

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,4 +1,5 @@
 import axios, { AxiosError, AxiosResponse, AxiosRequestConfig } from "axios";
+import { toast } from "react-toastify";
 import Config from "../config";
 
 const instance = axios.create();
@@ -16,15 +17,35 @@ instance.interceptors.request.use((config: AxiosRequestConfig) => {
 
 instance.interceptors.response.use(
   function (response: AxiosResponse) {
+    toast.dismiss();
     onLoadingChanged(false);
     return response;
   },
   function (error: AxiosError) {
+    toast.dismiss();
     onLoadingChanged(false);
+    showApiError(error);
     //TODO lging errors
     return Promise.reject(error);
   }
 );
+
+const showApiError = (error: AxiosError) => {
+  const expectedError =
+    error.response &&
+    error.response.status >= 400 &&
+    error.response.status <= 500;
+
+  if (expectedError) {
+    if (error.response?.status === 400) toast.error(`${error.response.data}`);
+    if (error.response?.status === 404) toast.error(`Not Found`);
+    else if (error.response?.status === 500)
+      toast.error("Internal Server Error");
+  } else {
+    if ((error + "").includes("Network")) toast.error("Network Error");
+    else toast.error("UnExpected Error");
+  }
+};
 
 export const onLoadingChangedEvent = (callback: (status: boolean) => void) => {
   onLoadingChanged = callback;
