1446479daf150ec7002aa5258135e479bed51f43
Meisam Malekzadeh <meisammalekzadeh@gmail.com>
2020-07-17 11:25:05 +0430

    removing player added-savetoken removed

--- src/services/roomService.ts
+++ src/services/roomService.ts
@@ -1,28 +1,31 @@
 import api from "./api";
 import { Room } from "./../types/room";
 
-const SUB_URL = "rooms/";
+const SUB_URL = "rooms";
 
 export const createRoom = async (name: string) => {
   const { data } = await api.post(SUB_URL, { name: name });
-  console.log('data' , data)
-  saveToken(data);
+  return data;
 };
 
 export const joinRoom = async (name: string, code: string) => {
-  const { data } = await api.patch(`${SUB_URL}/${code}`, { name: name });
-  saveToken(data);
+  const { data } = await api.put(`${SUB_URL}/${code}`, { name: name });
+  return data;
 };
 
 export const getRoom = async () => {
-  const { data } = await api.get(SUB_URL);
-  return data as Room;
+  try {
+    const { data } = await api.get(SUB_URL);
+    return data as Room;
+  } catch {
+    return null;
+  }
 };
 
-export const leaveRoom = async () => {
-    await api.put(`${SUB_URL}/leave`);
-}
+export const leaveRoom = async (index: number | null) => {
+  await api.patch(`${SUB_URL}/leave/${index !== null ? index : ""}`);
+};
 
-const saveToken = (token: any) => {
+export const saveToken = (token: any) => {
   localStorage.setItem("token", token);
 };
