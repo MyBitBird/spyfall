be3e2863ccc50dafc24b241303ec044c17e724e2
Meisam Malekzadeh <meisammalekzadeh@gmail.com>
2020-07-18 12:11:14 +0430

    using theme and language context

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,35 @@
 import React from "react";
-import { CssBaseline } from "@material-ui/core";
-import {Route,Switch} from 'react-router-dom'
+import { CssBaseline,ThemeProvider } from "@material-ui/core";
+import { Route, Switch } from "react-router-dom";
 import routes from "./routes";
-import RoomProvider from './providers/room'
-import GameProvider from './providers/game';
+import RoomProvider from "./providers/room";
+import GameProvider from "./providers/game";
+import LanguageProvider from "./providers/language";
 import Loading from "./components/loading";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import theme from './theme'
 
 function App() {
   return (
     <div className="App">
+      
       <CssBaseline />
+      <ThemeProvider theme={theme}>
       <Loading />
       <ToastContainer />
-      <RoomProvider>
-        <GameProvider>
-        <Switch>
-          {routes.map((route, key) => {
-            return <Route key={key} {...route} />;
-          })}
-        </Switch>
-        </GameProvider>
-      </RoomProvider>
+      <LanguageProvider>
+        <RoomProvider>
+          <GameProvider>
+            <Switch>
+              {routes.map((route, key) => {
+                return <Route key={key} {...route} />;
+              })}
+            </Switch>
+          </GameProvider>
+        </RoomProvider>
+      </LanguageProvider>
+      </ThemeProvider>
     </div>
   );
 }
