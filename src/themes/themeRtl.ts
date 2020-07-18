a0ef0793a11fe6e7297865f60634b87d68645455
Meisam Malekzadeh <meisammalekzadeh@gmail.com>
2020-07-18 13:52:49 +0430

    using iransans font

--- src/themes/themeRtl.ts
+++ src/themes/themeRtl.ts
@@ -1,6 +1,6 @@
 import { createMuiTheme } from "@material-ui/core/styles";
 import ltr from "./themeLtr";
 
-const theme = createMuiTheme({ ...ltr, direction: "rtl" });
+const theme = createMuiTheme({ ...ltr, direction: "rtl",typography:{"fontFamily":"Iransans"} });
 
 export default theme;
