d0b31e0cf40876a1ded42eddd1828cc2308b3c18
Meisam Malekzadeh <meisammalekzadeh@gmail.com>
2020-07-18 15:15:56 +0430

    setting font rot ltr

--- src/themes/themeLtr.ts
+++ src/themes/themeLtr.ts
@@ -1,16 +1,14 @@
-import { createMuiTheme } from '@material-ui/core/styles';
-import lightBlue from '@material-ui/core/colors/lightBlue';
+import { createMuiTheme } from "@material-ui/core/styles";
+import lightBlue from "@material-ui/core/colors/lightBlue";
 const theme = createMuiTheme({
-    direction: "ltr",
+  direction: "ltr",
+  typography: { fontFamily: "Roboto" },
   palette: {
     primary: {
       main: lightBlue[500],
-      contrastText:'white'
-      
-    } 
+      contrastText: "white",
+    },
   },
 });
-
-
 
 export default theme;
