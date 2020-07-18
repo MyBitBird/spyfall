5498913f7c3b4426bd25a58c9d44b3c10e7934bb
Meisam Malekzadeh <meisammalekzadeh@gmail.com>
2020-07-18 16:20:31 +0430

    German added

--- src/local/index.ts
+++ src/local/index.ts
@@ -1,5 +1,6 @@
 import en from "./en.json";
 import fa from "./fa.json";
+import ge from "./ge.json";
 import rtl from "../themes/themeRtl";
 import ltr from "../themes/themeLtr";
 import { Theme } from "@material-ui/core";
@@ -13,10 +14,12 @@ interface Direction {
 
 export const languagesList: Direction[] = [
   { key: "en", value: en, direction: "ltr", theme: ltr },
+  { key: "ge", value: ge, direction: "ltr", theme: ltr },
   { key: "fa", value: fa, direction: "rtl", theme: rtl },
 ];
 
 export const languageOptions = [
   { key: "en", value: "En" },
+  { key: "ge", value: "Ge" },
   { key: "fa", value: "فا" },
 ];
