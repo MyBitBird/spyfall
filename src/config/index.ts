4ff0d78c9a10a3a8a7ed6ee301808dbcfff578b5
Meisam Malekzadeh <meisammalekzadeh@gmail.com>
2020-07-18 19:52:48 +0430

    online api addresses

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,6 +1,6 @@
 export default {
-  API_BASE_URL: "http://localhost:5500/api/",
-  SOCKET_END_POINT: "http://localhost:2000",
+  API_BASE_URL: "https://morning-brushlands-99211.herokuapp.com/api/",
+  SOCKET_END_POINT: "https://morning-brushlands-99211.herokuapp.com/",
   EVENTS: {
     PLAYERS_CHANGED: "playersChanged",
     GAME_STARTED: "gameStarted",
