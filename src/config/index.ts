8a2e7a93b56534f70ad8dec96f7c74ade8628b92
Meisam Malekzadeh <meisammalekzadeh@gmail.com>
2020-07-19 00:07:16 +0430

    config changed

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,6 +1,6 @@
 export default {
-  API_BASE_URL: "https://morning-brushlands-99211.herokuapp.com/api/",
-  SOCKET_END_POINT: "https://morning-brushlands-99211.herokuapp.com/",
+  API_BASE_URL: "http://localhost:2000/api/",
+  SOCKET_END_POINT: "http://localhost:2000",
   EVENTS: {
     PLAYERS_CHANGED: "playersChanged",
     GAME_STARTED: "gameStarted",
